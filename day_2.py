0e9c70bba84c9af89c71f211b9c12bf8ae3ab626
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2020-12-04 20:46:02 -0500

    Update day_2.py

--- day_2.py
+++ day_2.py
@@ -11,3 +11,18 @@ for line in fileinput.input(files='input.txt'):
     if count >= int(numbers[0]) and count <= int(numbers[1]):
         real_count += 1
 print(real_count)
+
+import fileinput
+count = 0
+for line in fileinput.input(files='input.txt'):
+    break_line = line.split()
+    numbers = break_line[0].split("-")
+    letters = break_line[1][:1]
+    scrambles = break_line[2]
+    letter_1 = int(numbers[0]) - 1
+    letter_2 = int(numbers[1]) - 1
+    print(letter_1, letter_2, letters, scrambles)
+    if scrambles[letter_1] != scrambles[letter_2]:
+        if scrambles[letter_1] == letters or scrambles[letter_2] == letters:
+            count += 1
+print(count)
